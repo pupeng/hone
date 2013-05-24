41e289dc089a06733b31db66e3f6d24a2c36c8c6
pengsun <pengsun@ubuntu-VirtualBox.(none)>
2013-05-23 20:40:44 -0400

    update setupdependency

--- DependentLibraries/setupdependency.bash
+++ DependentLibraries/setupdependency.bash
@@ -8,7 +8,7 @@ tar -xvf psutil-0.7.0.tar.gz
 cd psutil-0.7.0
 sudo python setup.py install
 cd ..
-rm -rf psutil-0.7.0
+sudo rm -rf psutil-0.7.0
 
 # estats library
 tar -xvf estats_userland-2.0.1.tar.gz

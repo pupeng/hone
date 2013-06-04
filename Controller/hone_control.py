376a71a511f62f227947318eef821171abdebe0a
pengsun <pengsun@cs.princeton.edu>
2013-06-04 10:11:58 -0400

    add placeholder for control jobs

---
+++ Controller/hone_control.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2011-2013 Peng Sun. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the COPYRIGHT file.
+
+# hone_control.py
+# a placeholder file for any control jobs HONE runtime generates

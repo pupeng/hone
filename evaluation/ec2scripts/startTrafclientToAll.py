ecb2e7b410a4abc30c7499f4ba07f3d99759e423
Peng Sun <pengsun@cs.princeton.edu>
2013-06-10 18:50:17 +0000

    update scripts

--- evaluation/ec2scripts/startTrafclientToAll.py
+++ evaluation/ec2scripts/startTrafclientToAll.py
@@ -1,10 +1,10 @@
 from subprocess import check_output, call
 
 destinations = [
-	'10.165.16.47',
-	'10.154.129.146',
-	'10.154.179.121',
-	'10.144.84.108']
+    '10.166.0.183',
+    '10.166.7.91',
+    '10.154.149.174',
+    '10.147.159.54']
 
 def main():
     selfIP = check_output('ifconfig | grep -b1 eth0 | grep inet | cut -b24-34', shell=True, executable='/bin/bash').rstrip()

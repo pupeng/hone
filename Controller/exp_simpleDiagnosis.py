38c90cdb31dedf41bdf385ac87f0c25e19d4ce20
Peng Sun <pengsun@cs.princeton.edu>
2013-06-11 10:25:49 -0400

    add application of diagnosis

--- Controller/exp_simpleDiagnosis.py
+++ Controller/exp_simpleDiagnosis.py
@@ -3,97 +3,59 @@
 # found in the COPYRIGHT file.
 
 # HONE application
-# simple diagnosis job
-# detect connections with low congestion window, and identify their shared links (if exist) as the bottleneck link
+# calculate the link utilization of current network
 
 from hone_lib import *
 
-def ConnQuery():
-    return (Select(['src']))
+def LinkQuery():
+    return (Select(['BeginDevice', 'BeginPort', 'EndDevice', 'EndPort']) *
+            From('LinkStatus') *
+            Every(2000))
 
+def SwitchStatsQuery():
+    return (Select(['switchId', 'portNumber', 'transmitBytes', 'receiveBytes', 'capacity', 'timestamp']) *
+            From('SwitchStatus') *
+            Every(2000))
 
-def TrafficMatrixQuery():
-    return (Select(['srcIP','dstIP','BytesSentOut','StartTimeSecs','ElapsedSecs','StartTimeMicroSecs','ElapsedMicroSecs']) *
-            From('HostConnection') *
-            Groupby(['srcIP','dstIP']) *
-            Every(5000))
+def JoinTables(x):
+    (links, switchStats) = x
+    links = links[0]
+    switchStats = switchStats[0]
+    switchDataDict = {}
+    for (device, port, transmitBytes, receiveBytes, capacity, timestamp) in switchStats:
+        if device not in switchDataDict:
+            switchDataDict[device] = {}
+        switchDataDict[device][port] = [(transmitBytes + receiveBytes), capacity, timestamp]
+    results = {}
+    for (deviceA, portA, deviceB, portB) in links:
+        results[(deviceA, portA, deviceB, portB)] = switchDataDict[deviceB][portB]
+    return results
 
-def SumBytesSentForHostPair(connsOfOneHostPair):
-    hostAIP = None
-    hostBIP = None
-    timestamps = []
-    bytesSum = []
-    for (srcIP, dstIP, bytesSent, startSecs, elapsedSecs, startMicrosecs, elapsedMicrosecs) in connsOfOneHostPair:
-        hostAIP = srcIP
-        hostBIP = dstIP
-        bytesSum.append(bytesSent)
-        timestamp = startSecs + elapsedSecs + startMicrosecs / 1000000.0 + elapsedMicrosecs / 1000000.0
-        timestamps.append(timestamp)
-    return [hostAIP, hostBIP, sum(bytesSum), average(timestamps)]
-
-def CalThroughput(newData, tpData):
-    activeHostPair = []
-    for hostPairData in newData:
-        (hostAIP, hostBIP, newAccumBytes, newTimestamp) = hostPairData
-        if (hostAIP, hostBIP) in tpData:
-            (lastTimestamp, lastAccumBytes, lastRate) = tpData[(hostAIP, hostBIP)]
+def CalculateRate(newData, tpData):
+    for link, linkStats in newData.iteritems():
+        (newAccumBytes, capacity, newTimestamp) = linkStats
+        if link in tpData:
+            (lastTimestamp, lastAccumBytes, lastRate, _) = tpData[link]
             if newTimestamp > lastTimestamp:
-                newRate = float(newAccumBytes - lastAccumBytes) / (newTimestamp - lastTimestamp)
+                newRate = float((newAccumBytes - lastAccumBytes) / (newTimestamp - lastTimestamp))
             else:
                 newRate = 0.0
         else:
-            newRate = float(newAccumBytes) / newTimestamp
-        tpData[(hostAIP, hostBIP)] = (newTimestamp, newAccumBytes, newRate)
-        activeHostPair.append((hostAIP, hostBIP))
-    inactiveHostPair = []
-    for key in tpData.iterkeys():
-        if key not in activeHostPair:
-            inactiveHostPair.append(key)
-    for key in inactiveHostPair:
-        del tpData[key]
+            newRate = float(newAccumBytes) / float(newTimestamp)
+        tpData[link] = (newTimestamp, newAccumBytes, newRate, capacity)
     return tpData
 
-# exponentially weighted moving average
-def EWMA(newTpData, lastTpData):
-    for hostPair, newData in newTpData.iteritems():
-        (newTimestamp, _, newRate) = newData
-        if hostPair in lastTpData:
-            (lastTimestamp, lastRate) = lastTpData[hostPair]
-        else:
-            (lastTimestamp, lastRate) = (0.0, 0.0)
-        timeDiff = newTimestamp - lastTimestamp
-        if timeDiff > 0:
-            newRate = (1.0 - math.exp(-timeDiff / K)) * newRate + math.exp(-timeDiff / K) * lastRate
-        else:
-            newRate = 0.0
-        lastTpData[hostPair] = (newTimestamp, newRate)
-    return lastTpData
-
-def FormatThroughputData(tpData):
-    ret = {}
-    for key, value in tpData.iteritems():
-        ret[key] = value[1]
-    return ret
-
-def AggTrafficMatrix(x):
-    globalTrafficMatrix = {}
-    for dataFromEachHost in x:
-        for hostPair, rate in dataFromEachHost.iteritems():
-            globalTrafficMatrix[hostPair] = rate
-    return globalTrafficMatrix
-
-def DisplayTrafficMatrix(x):
-    for hostPair, rate in x.iteritems():
-        print 'IP1:{0} IP2:{1} Rate:{2} Kbps'.format(hostPair[0], hostPair[1], rate * 8 / 1000.0)
-    print '*******************\n'
+def DisplayUtilization(x):
+    for link, linkRate in x.iteritems():
+        (_, _, rate, capacity) = linkRate
+        print 'link {0}'.format(link)
+        print 'Rate:{0} Kbps Capacity:{1} Kbps Utilization:{2}%'.format(rate * 8.0 / 1000.0, capacity, rate * 8.0 / 10.0 / capacity)
+        print '***************************************************'
+    print '###############################\n\n'
 
 def main():
-    stream = TrafficMatrixQuery()
-    stream = stream >> MapStreamSet(MapList(SumBytesSentForHostPair))
-    stream = stream >> ReduceStreamSet(CalThroughput, {})
-    stream = stream >> ReduceStreamSet(EWMA, {})
-    stream = stream >> MapStreamSet(FormatThroughputData)
-    stream = stream >> MergeHosts()
-    stream = stream >> MapStream(AggTrafficMatrix)
-    stream = stream >> MapStream(DisplayTrafficMatrix)
+    stream = MergeStreams(LinkQuery(), SwitchStatsQuery())
+    stream = stream >> MapStream(JoinTables)
+    stream = stream >> ReduceStream(CalculateRate, {})
+    stream = stream >> MapStream(DisplayUtilization)
     return stream

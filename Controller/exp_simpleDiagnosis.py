3e8f68a2de4ba0720425ca503123c0ca7df9ae59
pengsun <pengsun@cs.princeton.edu>
2013-06-11 15:47:43 -0400

    finish diagnosis application

--- Controller/exp_simpleDiagnosis.py
+++ Controller/exp_simpleDiagnosis.py
@@ -29,56 +29,48 @@ def FormatHostInfoToDict(x):
         result[hostId] = ip
     return result
 
+def JoinTables(x):
+    (routes, hostInfo) = x
+    routes = routes[0]
+    result = {}
+    for (hostAId, hostBId, path) in routes:
+        hostAIP = hostInfo[hostAId]
+        hostBIP = hostInfo[hostBId]
+        result[(hostAIP, hostBIP)] = path
+        # path is a list of dictionaries, in which key 'switch' is the switch ID, and key 'port' is the interface number
+    return result
+
+def FormatRouteToAString(path):
+    result = ''
+    for switchInfo in path:
+        result += '{0},{1}#'.format(switchInfo['switch'], switchInfo['port'])
+    return result
+
+def FindLongestCommonSubstring(data):
+    substr = ''
+    if len(data) > 1 and len(data[0]) > 0:
+        for i in range(len(data[0])):
+            for j in range(len(data[0]) - i + 1):
+                if j > len(substr) and all(data[0][i:i+j] in x for x in data):
+                    substr = data[0][i:i+j]
+    return substr
+
+def IdentifySharedLinks(x):
+    (ipPairRoutes, faultyFlowsFromHosts) = x
+    problemLinks = []
+    for flowOnEachHost in faultyFlowsFromHosts:
+        for (src, dst, _) in flowOnEachHost:
+            problemLinks.append(ipPairRoutes[(src, dst)])
+    problemLinks = map(FormatRouteToAString, problemLinks)
+    bottleneckLinks = FindLongestCommonSubstring(problemLinks)
+    return bottleneckLinks
+
 def main():
     hostInfoStream = HostInfoQuery() >> MergeHosts() >> MapStream(FormatHostInfoToDict)
-    stream = hostInfoStream >> Print()
+    ipPairRouteStream = MergeStreams(RouteQuery(), hostInfoStream) >> MapStream(JoinTables)
+    faultyFlowsStream = ConnQuery() >> MergeHosts()
+    stream = MergeStreams(ipPairRouteStream, faultyFlowsStream) >> MapStream(IdentifySharedLinks) >> Print()
     return stream
 
-
-def LinkQuery():
-    return (Select(['BeginDevice', 'BeginPort', 'EndDevice', 'EndPort']) *
-            From('LinkStatus') *
-            Every(2000))
-
-def SwitchStatsQuery():
-    return (Select(['switchId', 'portNumber', 'transmitBytes', 'receiveBytes', 'capacity', 'timestamp']) *
-            From('SwitchStatus') *
-            Every(2000))
-
-def JoinTables(x):
-    (links, switchStats) = x
-    links = links[0]
-    switchStats = switchStats[0]
-    switchDataDict = {}
-    for (device, port, transmitBytes, receiveBytes, capacity, timestamp) in switchStats:
-        if device not in switchDataDict:
-            switchDataDict[device] = {}
-        switchDataDict[device][port] = [(transmitBytes + receiveBytes), capacity, timestamp]
-    results = {}
-    for (deviceA, portA, deviceB, portB) in links:
-        results[(deviceA, portA, deviceB, portB)] = switchDataDict[deviceB][portB]
-    return results
-
-def CalculateRate(newData, tpData):
-    for link, linkStats in newData.iteritems():
-        (newAccumBytes, capacity, newTimestamp) = linkStats
-        if link in tpData:
-            (lastTimestamp, lastAccumBytes, lastRate, _) = tpData[link]
-            if newTimestamp > lastTimestamp:
-                newRate = float((newAccumBytes - lastAccumBytes) / (newTimestamp - lastTimestamp))
-            else:
-                newRate = 0.0
-        else:
-            newRate = float(newAccumBytes) / float(newTimestamp)
-        tpData[link] = (newTimestamp, newAccumBytes, newRate, capacity)
-    return tpData
-
-def DisplayUtilization(x):
-    for link, linkRate in x.iteritems():
-        (_, _, rate, capacity) = linkRate
-        print 'link {0}'.format(link)
-        print 'Rate:{0} Kbps Capacity:{1} Kbps Utilization:{2}%'.format(rate * 8.0 / 1000.0, capacity, rate * 8.0 / 10.0 / capacity)
-        print '***************************************************'
-    print '###############################\n\n'
 
 
